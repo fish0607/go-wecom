82365bce944ad3905e4f0af405c52ade9f33460d
yuj0607 <yuj0607@163.com>
2025-01-13 11:36:39 +0800

    Update client_contacts_dept_test.go

--- wecom/client_contacts_dept_test.go
+++ wecom/client_contacts_dept_test.go
@@ -4,5 +4,5 @@ func init() {
 	registerClientAPIPath("/cgi-bin/department/create", "CreateDepartment", cRef.CreateDepartment)
 	registerClientAPIPath("/cgi-bin/department/update", "UpdateDepartment", cRef.UpdateDepartment)
 	registerClientAPIPath("/cgi-bin/department/delete", "DeleteDepartment", cRef.DeleteDepartment)
-	registerClientAPIPath("/cgi-bin/department/list", "ListDepartment", cRef.ListDepartment)
+	registerClientAPIPath("/cgi-bin/department/simplelist", "ListDepartment", cRef.ListDepartment)
 }
